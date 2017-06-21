73d1aa000376306a563a1b26d1b5edbdd65a1a48
evkapsal <vkapsalakis@windowslive.com>
2017-06-22 00:11:02 +0300

    IT ASSET MANAGEMENT BETA 2.4
    Azure Forms, Views, Scripts Ready, Minor Bugs Resolved

---
+++ Scripts/2016/Clean Environment.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module smlets
+
+#Remove System Software
+$SoftwareCl= Get-SCSMClass -Name System.SoftwareItem$
+$SoftObjs= Get-SCSMObject -Class $SoftwareCl
+foreach ($SoftObj in $Softobjs)
+{Remove-SCSMObject -SMObject $SoftObj -Force}
+
+#Remove Hardware Computers
+$HardCl= Get-SCSMClass -Name Hardware$ | ? {$_.ManagementPack -eq "IT_Asset_Management_Base"}
+$HardwareItems= Get-SCSMObject -Class $HardCl
+foreach($HardwareItem in $HardwareItems)
+{Remove-SCSMObject -SMObject $HardwareItem -Force}
+
+#Remove Hardware Assets
+$HardAsCL= Get-SCSMClass -Name HardwareAsset$
+$HardwareAssets= Get-SCSMObject -Class $HardAsCL
+foreach($HardwareAsset in $HardwareAssets)
+{Remove-SCSMObject -SMObject $HardwareAsset -Force}
+
+#Remove Publishers
+$PubCL= Get-SCSMClass -Name SoftwarePublisher$
+$Publishers = Get-SCSMObject -Class $PubCL
+foreach($Publisher in $Publishers)
+{Remove-SCSMObject -SMObject $Publisher -Force}
+
+#Remove Versions
+$VersionCL= Get-SCSMClass -Name SoftwareVersion$
+$Versions= Get-SCSMObject -Class $VersionCL
+foreach($Version in $Versions)
+{Remove-SCSMObject -SMObject $Version -Force}
+
+#Remove Software
+$SoftCL= Get-SCSMClass -Name Software$
+$Softwares= Get-SCSMObject -Class $SoftCL
+foreach($Software in $Softwares)
+{Remove-SCSMObject -SMObject $software -Force}
+
+#Remove Software Asset
+$SftAssCl= Get-SCSMClass -Name SoftwareAsset$
+$SoftAssets= Get-SCSMObject -Class $SftAssCl
+foreach($SoftAsset in $SoftAssets)
+{Remove-SCSMObject -SMObject $SoftAsset -Force}
+
+
---
+++ Scripts/2016/SoftScan.ps1
@@ -0,0 +1,73 @@
+﻿Import-Module smlets
+$wincl= Get-SCSMClass -Name Windows.Computer$
+$comps= Get-SCSMObject -Class $wincl
+$SoftwareClass=  Get-SCSMClass -Name System.SoftwareItem$
+$Active= Get-SCSMEnumeration -Name System.ConfigItem.AssetStatusEnum.Deployed
+$HasSoftwareInstalled= Get-SCSMRelationshipClass -Name System.DeviceHasSoftwareItemInstalled$
+
+foreach ($comp in $comps)
+{
+
+Try
+	{
+	$Test= Test-Connection -ComputerName $comp.displayname
+	}
+catch
+	{
+	$Test= $null
+	}
+finally{
+	if($Test)
+		{
+		$Apps = $null
+		$Apps= gwmi Win32_Product -ComputerName $comp | ?{$_.InstallState -eq 5}
+		
+
+			ForEach ($Software in $Apps)
+			{
+				$Name= $Software.Name
+				$Version= $Software.Version
+				$PackageCode= $Software.PackageCode
+				[int]$Language= $Software.Language
+				$Vendor= $Software.Vendor
+				
+				$Hashtable= @{
+					ProductName= $Name;
+					DisplayName= $Name; 
+					VersionString= $Version;
+					ProductCode= $PackageCode;
+					LocaleID= $Language;
+					IsVirtualApplication=$false;
+					Publisher= $Vendor;
+					MajorVersion= $Version
+					MinorVersion= $Version
+					AssetStatus= $Active
+					}
+	try{
+		$HasBeenInserted=$null
+		$HasBeenInserted= Get-SCSMObject -Class $SoftwareClass -Filter "DisplayName -eq '$Name' -and VersionString -eq '$Version'"
+		}
+	finally{
+	if(!$HasBeenInserted){
+			
+			$NewSoftware=$null
+			$NewSoftware= New-SCSMObject -Class $SoftwareClass -PropertyHashtable $Hashtable -PassThru
+			$AssignRelationship= New-SCSMRelationshipObject -Relationship $HasSoftwareInstalled -Source $comp -Target $NewSoftware -Bulk	
+						 }
+	else{
+		$GetRel=$null
+		$GetRel= Get-SCSMRelationshipObject -BySource $comp |? {$_.TargetObject.DisplayName -eq $HasBeenInserted.DisplayName}
+		if(!$GetRel)
+			{
+			$AssignRelationship= New-SCSMRelationshipObject -Relationship $HasSoftwareInstalled -Source $comp  -Target $HasBeenInserted -Bulk
+			}
+		}
+	
+	}
+	
+	
+			}
+		}
+	}
+
+}
